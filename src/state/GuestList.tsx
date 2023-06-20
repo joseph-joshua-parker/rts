8d82e80756ecbb30092deb283bf7ef115d7bf703
joseph-joshua-parker <joelnashjobs@yahoo.com>
2023-06-20 12:17:37 -0300

    Lesson 303: Annotating type of state when its an object

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -3,11 +3,24 @@ import { useState } from "react";
 const GuestList: React.FC = ()=> {
 	const [name, setName] = useState('');
 
+	const [guests, setGuests] = useState<string[]>([]);
+								//We must manually annotate state type when 
+								//initializing as an empty object
+
+
+	const addGuest = ()=>{
+		setName('');
+		setGuests([...guests, name])
+	}
 
 	return <div>
 		<h3>Guest List</h3>
+		<ul>
+			{guests.map(guest=> <li>{guest}</li>)}
+		</ul>
+		
 		<input value={name} onChange={(e)=>setName(e.target.value)}/>
-		<button>Add Guest</button>
+		<button onClick={addGuest}>Add Guest</button>
 	</div>
 }
 
