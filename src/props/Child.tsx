2a5c7063252e38c7b1d270a423757ffdc99b4dfa
joseph-joshua-parker <joelnashjobs@yahoo.com>
2023-06-20 11:44:37 -0300

    Lesson 297: Simple way to type FC props, and problem

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,4 +1,14 @@
-export const Child = ()=>{
+
+//Given the following interface
+interface ChildProps {
+	color: string
+}
+
+					//Simple annotation
+export const Child = ({color}:ChildProps)=>{
 	return <div>Some</div>
 }
+
+//PROBLEM: TypeScript doesn't understand that this is specifically a REACT component, meaning
+//optional props unique to React won't be accepted unless they are hard coded into the interface
 
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,7 @@
 import {Child} from './Child';
 
 const Parent = () => {
-	return <Child/>
+	return <Child color="blue"/>
 }
 
 export default Parent
