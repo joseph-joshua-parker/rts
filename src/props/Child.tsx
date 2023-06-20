fea7c4d945a531e4f15602b6188eee3635ab34b3
joseph-joshua-parker <joelnashjobs@yahoo.com>
2023-06-20 11:55:34 -0300

    Lesson 300: Proper prop annotation

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,7 +1,10 @@
+import { ReactNode } from "react"
 
 //Given the following interface
 interface ChildProps {
 	color: string
+	//In React 18 we must manually add children to the interface
+	children: ReactNode;
 }
 
 					//Simple annotation
@@ -12,3 +15,16 @@ export const Child = ({color}:ChildProps)=>{
 //PROBLEM: TypeScript doesn't understand that this is specifically a REACT component, meaning
 //optional props unique to React won't be accepted unless they are hard coded into the interface
 
+
+					//'Proper' annotation
+export const ChildAsFC: React.FC<ChildProps> = ({color, children}) =>{
+	return <div>
+		{color}
+		{children}
+
+	</div>
+}
+
+//ADVANTAGE: Typescript now understands that it is a React Component, and additional
+//optional properties are correctly accepted without having to be manually added,
+//with the exception of the children prop
