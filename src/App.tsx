c90a394337325b9c1fa882846ce4ab74bc55dc76
joseph-joshua-parker <joelnashjobs@yahoo.com>
2023-06-20 12:00:02 -0300

    Lesson 301: State with TypeScript: Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import GuestList from './state/GuestList';
 
 function App() {
   return (
     <div className="App">
-
+      <GuestList/>
 
     </div>
   );
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,7 @@
 import {Child} from './Child';
 
 const Parent = () => {
-	return <Child color="blue"/>
+	return <Child color="blue">some text</Child>
 }
 
 export default Parent
