41c4ca3d51fec8d953e4767e160e46d6ffac1ae2
joseph-joshua-parker <joelnashjobs@yahoo.com>
2023-06-20 12:29:31 -0300

    Lesson 305: State which can be undefined: User Search

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
 
-const Users = [
+const users = [
 	{name: 'Sarah', age: 20},
 	{name: 'Alex', age: 21}, 
 	{name: 'Michael', age: 28}
 ];
 
+interface FoundUser {
+	name: string,
+	age: number
+}
 
 const UserSearch: React.FC = ()=>{
-	const [name, setName] = useState('');
+	const [name, setName] = useState(''); //Since sometimes no matching user is found, allow for undefined
+	const [foundUser, setFoundUser] = useState<FoundUser | undefined>();
+
 	const handleSearch = ()=>{
-		
+		const user = users.find(user => user.name === name)
+		setFoundUser(user);
 	}
 	return <div>User Search
 		<input value={name} onChange={e=>setName(e.target.value)}/>
 		<button onClick={handleSearch}></button>
+		<div>
+
+			{/*undefined guard using && */}
+			{foundUser && foundUser.name}
+			{foundUser && foundUser.age}
+		</div>
 	</div>
 }
 
